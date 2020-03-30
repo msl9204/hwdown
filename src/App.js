299743ad6ee01ade99a7d3ae4e2a7140108f2c73
Lee <letomok@gmail.com>
2020-03-30 06:47:26 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import lion from "./lion.png";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={lion} className="App-logo" alt="logo" />
+                <p>
+                    <strong>Whirling Lion</strong>
+                </p>
+                <a
+                    className="App-link"
+                    href="https://tahiti.mju.ac.kr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Go Tahiti
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
